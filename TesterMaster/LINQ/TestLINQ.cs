51c4c74bd6757786d05fd142b5d76df0ca9d21a0
Danil <75370668+danilselitskiy@users.noreply.github.com>
2021-12-28 15:54:39 +0300

    Add linq test (#3)
    * Use var
    
    * Add power shell
    
    * Update ps scripts
    
    * Add dictionary
    
    * Add linq test

---
+++ TesterMaster/LINQ/TestLINQ.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TestMasterInfrastructure.Interfaces;
+
+namespace TesterMaster.LINQ
+{
+    public class TestLINQ : ITest
+    {
+        private class Item
+        {
+            public Item(float originalPrice, float discountedPrice)
+            {
+                OriginalPrice = originalPrice;
+                DiscountedPrice = discountedPrice;
+            }
+
+            public float OriginalPrice { get; set; }
+            public float DiscountedPrice { get; set; }
+        }
+
+        private readonly List<Item> Items = new List<Item>()
+        {
+            new Item (10,7),
+            new Item (12,10),
+            new Item (11,11)
+        };
+
+        public void StartTest()
+        {
+            TestSelectAndSumFloat();
+        }
+
+        private void TestSelectAndSumFloat()
+        {
+            Console.WriteLine($"Start case 1: test select with sum of float values");
+
+            try
+            {
+                var totalDiscount = Items.Select(item => item.OriginalPrice - item.DiscountedPrice).Sum();
+                Console.WriteLine($"Total discount: {totalDiscount}");
+            }
+            catch { }
+
+            Console.WriteLine($"End case 1.");
+        }
+    }
+}
